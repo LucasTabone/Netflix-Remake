93ee51a5a83fac33e06fc55d160fcad6a74c2e0f
Lucas Tabone <Lucas.tabone@hotmail.com>
2022-12-16 15:14:00 -0300

    BackButton, Films Details - 008

--- app/src/main/java/co/tiagoaguiar/netflixremake/MovieActivity.kt
+++ app/src/main/java/co/tiagoaguiar/netflixremake/MovieActivity.kt
@@ -3,16 +3,22 @@ package co.tiagoaguiar.netflixremake
 import android.graphics.drawable.LayerDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.MenuItem
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import co.tiagoaguiar.netflixremake.Util.MovieTask
 import co.tiagoaguiar.netflixremake.model.Movie
+import co.tiagoaguiar.netflixremake.model.MovieDetail
 import org.w3c.dom.Text
+import java.lang.IllegalStateException
 
-class MovieActivity : AppCompatActivity() {
+class MovieActivity : AppCompatActivity(), MovieTask.Callback {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie)
@@ -21,6 +27,11 @@ class MovieActivity : AppCompatActivity() {
         val txtDesc: TextView = findViewById(R.id.movie_txt_desc)
         val txtCast: TextView = findViewById(R.id.movie_txt_cast)
         val rv: RecyclerView = findViewById(R.id.movie_rv_similar)
+
+        val id = intent?.getIntExtra("id",0) ?: throw IllegalStateException("ID não foi encontrado!")
+        val url = "https://api.tiagoaguiar.co/netflixapp/movie/$id?apiKey=7a2bc5d7-0b11-49ec-84a4-fea0b48e51fd"
+
+        MovieTask(this).execute(url)
 
         txtTitle.text = "Batman Begins"
         txtDesc.text = "esse é o filme do Batmans begins"
@@ -43,6 +54,25 @@ class MovieActivity : AppCompatActivity() {
         layerDrawable.setDrawableByLayerId(R.id.cover_drawable, movieCover)
         val coverImg: ImageView = findViewById(R.id.movie_img)
         coverImg.setImageDrawable(layerDrawable)
+    }
+
+    override fun onPreExecute() {
 
     }
+
+    override fun onFailure(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onResult(movieDetail: MovieDetail) {
+        Log.i ("Teste", movieDetail.toString())
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home){
+            finish()
+            }
+        return super.onOptionsItemSelected(item)
+    }
+
 }
